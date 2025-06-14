fc2aa1955b90ff6d0864da447f220d66cb625deb
Official-Kallol <official.kallol2024@gmail.com>
2025-06-14 23:52:45 +0530

    setup nested dynamic routes function

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,12 @@
+export default async function ReviewDetails({
+  params,
+}: {
+  params: Promise<{ productId: string; reviewId: string }>;
+}) {
+  const { productId, reviewId } = await params;
+  return (
+    <div>
+      <h1>Review details {reviewId} for product {productId}</h1>
+    </div>
+  );
+}
